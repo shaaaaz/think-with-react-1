da9d9b21e3579c2562696477295f6181bc632c5b
shaaaaz <jiwanishaaz@gmail.com>
2024-01-15 10:09:22 +0530

    push

---
+++ Progression 1/app.js
@@ -0,0 +1,19 @@
+// const container = document.getElementById('react-container')
+
+// const Container = () => {
+//     return React.createElement('div',null,'"Hey Kalvians! Welcome to React Learning!',
+//     React.createElement('div',null,'Lets rock and roll'))
+// }
+
+// ReactDOM.render(React.createElement(Container),container)
+
+class ReactContainer extends React.Component{
+      render(){
+          return React.createElement(`div`,null,`Hey Kalvians`,
+          React.createElement(`div`,null,`Let's rock and roll with classes`)
+          );
+      }
+    
+  }
+  const container = document.getElementById('react-container');
+  ReactDOM.render(React.createElement(ReactContainer),container);
